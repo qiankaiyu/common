528242b03181de279dc5b39f74b0f1c329370db7
chuck <986022405@qq.com>
2021-10-23 18:30:11 +0800

    feat：ContextConfig

--- src/main/java/com/chuck/common/config/ContextConfig.java
+++ src/main/java/com/chuck/common/config/ContextConfig.java
@@ -1,21 +1,33 @@
 package com.chuck.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.Resource;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
 
 /**
  *
@@ -32,6 +44,13 @@ public class ContextConfig implements WebMvcConfigurer {
     @Resource
     @Setter
     private SpringValidatorAdapter validator;
+    @Resource
+    @Setter
+    private ObjectMapper objectMapper;
+    @Value("${com.chuck.fileLocation}")
+    private String fileLocation;
+    @Autowired
+    private StorageProperties storageProperties;
 
     /**
      * 替代SpringMVC自行创建的Validator实例，用于表单对象参数的验证
@@ -41,22 +60,19 @@ public class ContextConfig implements WebMvcConfigurer {
         return this.validator;
     }
 
-    @Resource
-    @Setter
-    private ObjectMapper objectMapper;
-
     /**
-     * 仅支持Json的MessageConverters，重载该方法将会去除之前所有预置的Convert（
+     * 仅支持Json的MessageConverters
      */
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        converters.add(new SourceHttpMessageConverter<>());
-//        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-//        jsonConverter.setSupportedMediaTypes(
-//                Arrays.asList(new MediaType("application", "json"), new MediaType("text", "json")));
-//        jsonConverter.setObjectMapper(this.objectMapper);
-//        converters.add(jsonConverter);
-//    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new SourceHttpMessageConverter<>());
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setSupportedMediaTypes(
+            Arrays.asList(new MediaType("application", "json"), new MediaType("text", "json")));
+        jsonConverter.setObjectMapper(this.objectMapper);
+        converters.add(jsonConverter);
+        converters.add(new ResourceHttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -64,7 +80,25 @@ public class ContextConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public LocaleResolver localResolver(){
+    public LocaleResolver localeResolver() {
         return new AcceptHeaderLocaleResolver();
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/files/**").addResourceLocations("classpath:/static/" );
+//    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper helper = new UrlPathHelper();
+        helper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(helper);
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler(ServletUriComponentsBuilder.fromCurrentContextPath().path("/files/")
+//            .toUriString()).addResourceLocations("file:"+storageProperties.getFileLocation());
+//    }
 }
