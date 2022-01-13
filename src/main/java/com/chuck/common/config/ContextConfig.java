33d7464fbc01e85c711ca77e98d02b10934a3675
qiankaiyu <986022405@qq.com>
2022-01-13 15:45:24 +0800

    feat（gitignore）：common config

--- src/main/java/com/chuck/common/config/ContextConfig.java
+++ src/main/java/com/chuck/common/config/ContextConfig.java
@@ -26,91 +26,97 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
 /**
- *
  * @author Chuck
  * @since 7/5/2021
  * @version 0.0.1
  * @apiNote
- **/
+ */
 @Configuration
 @EnableWebMvc
-@EnableTransactionManagement(mode = AdviceMode.PROXY, proxyTargetClass = false, order = Ordered.LOWEST_PRECEDENCE)
+@EnableTransactionManagement(
+    mode = AdviceMode.PROXY,
+    proxyTargetClass = false,
+    order = Ordered.LOWEST_PRECEDENCE)
 public class ContextConfig implements WebMvcConfigurer {
 
-    @Resource
-    @Setter
-    private SpringValidatorAdapter validator;
-    @Resource
-    @Setter
-    private ObjectMapper objectMapper;
-    @Value("${com.chuck.fileLocation}")
-    private String fileLocation;
-    @Autowired
-    private StorageProperties storageProperties;
+  @Resource @Setter private SpringValidatorAdapter validator;
+  @Resource @Setter private ObjectMapper objectMapper;
 
-    /**
-     * 替代SpringMVC自行创建的Validator实例，用于表单对象参数的验证
-     */
-    @Override
-    public Validator getValidator() {
-        return this.validator;
-    }
+  @Value("${com.chuck.fileLocation}")
+  private String fileLocation;
 
-    /**
-     * 仅支持Json的MessageConverters
-     */
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new SourceHttpMessageConverter<>());
-        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        jsonConverter.setSupportedMediaTypes(
-            Arrays.asList(new MediaType("application", "json"), new MediaType("text", "json")));
-        jsonConverter.setObjectMapper(this.objectMapper);
-        converters.add(jsonConverter);
-        converters.add(new ResourceHttpMessageConverter());
-    }
+  @Autowired private StorageProperties storageProperties;
 
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
+  /** 替代SpringMVC自行创建的Validator实例，用于表单对象参数的验证 */
+  @Override
+  public Validator getValidator() {
+    return this.validator;
+  }
 
-//    @Bean
-//    public LocaleResolver localeResolver() {
-//        return new AcceptHeaderLocaleResolver();
-//    }
+  /** 仅支持Json的MessageConverters */
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(new SourceHttpMessageConverter<>());
+    MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+    jsonConverter.setSupportedMediaTypes(
+        Arrays.asList(new MediaType("application", "json"), new MediaType("text", "json")));
+    jsonConverter.setObjectMapper(this.objectMapper);
+    converters.add(jsonConverter);
+    converters.add(new ResourceHttpMessageConverter());
+  }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/" );
-//        registry.addResourceHandler("/files/**").addResourceLocations("classpath:/static/" );
-//    缓存静态内容
-//        registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/" ).setCachePeriod(365*24*60*60);
-//    启用Gzip压缩
-//        registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/" ).resourceChain(true).addResolver(new GzipResourceResolver()).addResolver(new PathResourceResolver());
-//    }
+  @Override
+  public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.defaultContentType(MediaType.APPLICATION_JSON);
+  }
 
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        UrlPathHelper helper = new UrlPathHelper();
-        helper.setRemoveSemicolonContent(false);
-        configurer.setUrlPathHelper(helper);
-    }
+  //    @Bean
+  //    public LocaleResolver localeResolver() {
+  //        return new AcceptHeaderLocaleResolver();
+  //    }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler(ServletUriComponentsBuilder.fromCurrentContextPath().path("/files/")
-//            .toUriString()).addResourceLocations("file:"+storageProperties.getFileLocation());
-//    }
+  //    @Override
+  //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  //
+  // registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/"
+  // );
+  //        registry.addResourceHandler("/files/**").addResourceLocations("classpath:/static/" );
+  //    缓存静态内容
+  //
+  // registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/"
+  // ).setCachePeriod(365*24*60*60);
+  //    启用Gzip压缩
+  //
+  // registry.addResourceHandler("/static-resources/**").addResourceLocations("/static-resources/"
+  // ).resourceChain(true).addResolver(new GzipResourceResolver()).addResolver(new
+  // PathResourceResolver());
+  //    }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    UrlPathHelper helper = new UrlPathHelper();
+    helper.setRemoveSemicolonContent(false);
+    configurer.setUrlPathHelper(helper);
+  }
 
-        registry.addResourceHandler( "api/swagger-ui.html" )
-            .addResourceLocations( "classpath:/META-INF/resources/" );
-    }
+  //    @Override
+  //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  //
+  // registry.addResourceHandler(ServletUriComponentsBuilder.fromCurrentContextPath().path("/files/")
+  //            .toUriString()).addResourceLocations("file:"+storageProperties.getFileLocation());
+  //    }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+    registry
+        .addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    registry
+        .addResourceHandler("api/swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+  }
 }
