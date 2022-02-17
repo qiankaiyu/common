bbdd6bd80b66c3c2dd032d17be99ae50f5d297ad
qiankaiyu <986022405@qq.com>
2022-02-17 09:26:58 +0800

    feat（bootJar）：增加bootJar任务

---
+++ src/main/java/com/chuck/common/Application.java
@@ -0,0 +1,17 @@
+package com.chuck.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Starter
+ *
+ * @author Frank Zhang
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
